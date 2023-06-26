7827dbd73b61e69e2a787d26ae8d850bc132e419
Aditya Agarwal <adi790u@gmail.com>
2023-06-26 18:19:06 +0530

    Initialized Chat app

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const url = process.env.URL;
+const dbStart = require('./config/db');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/v1/users', require('./routes/userRoutes'));
+
+app.get('/', (req, res) => {
+  res.json({ msg: 'Api working...' });
+});
+
+dbStart(url);
+
+app.listen(port, () => {
+  console.log('Server running');
+});
